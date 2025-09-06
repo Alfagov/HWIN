1d138c8fc214ce27c59556f4ebeb53a039e64207
alfagov <lorenzopulcini@protonmail.ch>
2025-09-06 14:06:38 -0500

    Initial Commit

---
+++ GeriHealth/GeriHealthApp.swift
@@ -0,0 +1,32 @@
+//
+//  GeriHealthApp.swift
+//  GeriHealth
+//
+//  Created by Lorenzo P on 9/6/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GeriHealthApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
